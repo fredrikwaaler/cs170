40431b1ba94401cf4feb4d182bd1095c102392d3
fredrikwaaler <fredrik.waaler@gmail.com>
2019-11-19 18:48:00 -0800

    first commit

---
+++ input_generator.py
@@ -0,0 +1,17 @@
+import random
+# Number of locations
+num_loc = input("Number of locations:")
+# Number of homes
+num_homes = random.randint()
+# Location names (a-z, A-Z, max 20 chars)
+
+# Home names (subset of location)
+
+# Start/end location
+
+# One line for each location - entry  j in line i marks the distance of edge from node i to j. 'x' if no edge.
+# index i,i should also be 'x', no edge from a node to itself. # Distances max value 2bil, max 5 decimals.
+# Graph must be connected - any line needs an entry to represent an edge from every node.
+# Enforce triangle inequality
+
+
